afbe8753f20dcd78db205fbdb51989cce5e20f8a
Massimo Vezzelli <vissernk@gmail.com>
2021-06-16 00:30:54 +0200

    Update composer_setup.sh

--- composer_setup.sh
+++ composer_setup.sh
@@ -6,5 +6,6 @@ mkdir silverstripe-cache
 chown -R www-data:www-data silverstripe-cache
 chmod -R 777 silverstripe-cache
 chmod -R 777 assets
+chown www-data:www-data vendor
 ./vendor/bin/sake installsake
 sake dev/tasks/DemoResetTask
